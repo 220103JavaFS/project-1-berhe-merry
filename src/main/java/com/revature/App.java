febe0ee4f43b044b903cb652b736e7c6dce36ea8
merry-degaga <merry.degaga@revature.net>
2022-01-22 08:06:46 -0500

    add abstract controller class and App standard code

--- src/main/java/com/revature/App.java
+++ src/main/java/com/revature/App.java
@@ -1,4 +1,25 @@
 package com.revature;
+import com.revature.controllers.*;
+import io.javalin.Javalin;
 
 public class App {
+
+    private static Javalin app;
+
+    public static void main(String[] args) {
+
+        app = Javalin.create( c -> {
+            //c.accessManager(new AccessManagerConfigure());
+        }).start();
+
+        //configure(new LogonController());
+    }
+
+    public static void configure(Controller... controllers) {
+        for(Controller c: controllers){
+            c.addRoutes(app);
+        }
+    }
+
+
 }
