a6ed88b066db485d2439e96a44f4e68cf4313b9a
merry-degaga <merry.degaga@revature.net>
2022-01-22 12:59:53 -0500

    updated users model

--- src/main/java/com/revature/models/Users.java
+++ src/main/java/com/revature/models/Users.java
@@ -1,4 +1,87 @@
 package com.revature.models;
 
+import java.util.Objects;
+
 public class Users {
+
+    private String userName;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private int roleID;
+
+    public Users() {
+    }
+
+    public Users(String userName, String firstName, String lastName, String email, int roleID) {
+        this.userName = userName;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.roleID = roleID;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getRoleID() {
+        return roleID;
+    }
+
+    public void setRoleID(int roleID) {
+        this.roleID = roleID;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Users users = (Users) o;
+        return roleID == users.roleID && Objects.equals(userName, users.userName) && Objects.equals(firstName, users.firstName) && Objects.equals(lastName, users.lastName) && Objects.equals(email, users.email);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(userName, firstName, lastName, email, roleID);
+    }
+
+    @Override
+    public String toString() {
+        return "Users{" +
+                "userName='" + userName + '\'' +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", email='" + email + '\'' +
+                ", roleID=" + roleID +
+                '}';
+    }
 }
