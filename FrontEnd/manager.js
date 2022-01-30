23ee31a859034b35a7880327f3a2d68d5d1026f7
Author <beradiama@gmail.com>
2022-01-30 05:10:46 -0800

    FrontEnd first sample with js & html not Angular

---
+++ FrontEnd/manager.js
@@ -0,0 +1,178 @@
+
+
+const url = "http://localhost:8080/"
+
+if (sessionStorage.getItem("userSession") == null){
+    window.location.replace(url + "login.html");
+}else{
+    let user = sessionStorage.getItem("userSession");
+    if(user.userRoledId ==1){
+        window.location.replace(url + "employee.html");
+    }
+    else{
+      window.location.replace(url + "manager.html");
+  }
+}
+
+let statusUpdate = document.querySelector('#reimpType');
+let submitBtn = document.getElementById("updateStatusBtn");
+let reimTable = document.getElementById("reimTabl");
+let reimBtn = document.getElementById("getRequests");
+let loginBtn = document.getElementById("loginBtn");
+let filterUpdate = document.getElementById("#searchReimStatus");
+let getListBtn = document.getElementById("getList");
+
+
+submitBtn.addEventListener("click", setStatus);
+reimBtn.addEventListener("click", getAllRequests);
+logoutBtn.addEventListener("click", logoutFunc);
+getListBtn.addEventListener("click", getAllRequestsById);
+
+async function logoutFunc(){
+    
+  
+    let response = await fetch(
+      url+"logout",
+      {
+        method : "POST",   
+        credentials: "include"
+      }
+    );
+  
+    if(response.status===200){
+        sessionStorage.clear();
+        window.location.replace(url + "login.html");
+    }else{
+      console.log("Logout unsuccessful. Returned status code of:"+response.status);
+    }
+}
+
+async function setStatus() {
+   
+    // visual feedback of status update choice
+    output = statusUpdate.value;
+    document.querySelector('.output').textContent = output;
+
+    let statusId_value = 1;
+
+    if (document.querySelector('#reimStatus').value == "denied"){
+      statusId_value = 2;
+    } else if(document.querySelector('#reimStatus').value == "approved") {
+      statusId_value = 3;
+    } 
+
+    console.log(document.querySelector('#reimStatus').value);
+
+    // setting status update choice as value for put request
+    let status = {
+        reimId: parseInt(document.getElementById("reimId").value),
+
+        statusId: statusId_value
+    } 
+
+
+    let response = await fetch(url + "reimbursements", {
+        method:"PUT",
+        body:JSON.stringify(status),
+        credentials:"include"
+    })
+
+    if(response.status === 200){
+        getAllRequests(); 
+        console.log("Request status updated successfully!");
+    }else {
+        console.log("Request update failed! Try it again.");
+    }
+}
+
+// All reimbursement Requests' list
+async function getAllRequests(){
+    
+
+    let response = await fetch(url + "reimbursements", {
+      credentials:"include"
+
+    });
+  
+    if(response.status === 200){
+
+      let requests = await response.json();
+      populateRequests(requests);
+    
+    } else{
+
+      console.log("No reimbursement list found!")
+
+    }
+}
+
+// view reimbursement list by id
+async function getAllRequestsById(){
+  let statusId_value = 1;
+  
+  if (document.querySelector('#searchReimStatus').value == "denied"){
+      statusId_value = 2;
+  } else if(document.querySelector('#searchReimStatus').value == "approved") {
+      statusId_value = 3;
+  } 
+
+  let response = await fetch(url + "reimbursements/" + statusId_value , {
+    credentials:"include"
+  });
+
+  if(response.status === 200){
+
+    let requests = await response.json();
+    populateRequests(requests);
+  
+  } else{
+
+    console.log("No reimbursement list found!");
+
+  }
+}
+
+// create rows and fill them with data from reimbursement request
+function populateRequests(requests){
+
+    reimTable.innerHTML ="";
+
+    for(let request of requests){
+
+      let row = document.createElement("tr");
+
+      for(let data in request){
+        // We're skipping the information we don't need to show
+        if(data != "authorId" && data != "resolverId" && data != "receipt" && data != "statusId" && data != "typeId"){            
+          // data = Key
+          // request[data] = Value
+          let request_data = request[data];
+          // We need to format the dates that come from our request
+          if (data == "dateSubmitted" || data == "dateResolved") {       
+            request_data = formatDate(request_data);
+          }
+          let td = document.createElement("td");
+          td.innerText = request_data;
+          row.appendChild(td); 
+        }
+      }
+      reimTable.appendChild(row); 
+    }
+}
+
+function formatDate(dateData){
+  // Our Result
+  var result = "";
+  // Ensures data is not null
+  if (dateData != null) {
+    // Creates new Date from data
+    var d = new Date(dateData);
+    // Uses ternary operator
+    var ampm = (d.getHours() >= 12) ? "PM" : "AM";    
+    // Creates our formatting
+    result = ((d.getMonth() + 1) + "/"  + d.getDate() + "/" + d.getFullYear() +
+      " " + ((d.getHours() + 11) % 12 + 1) + ":" + d.getMinutes() + " " + ampm);
+  }
+  // Returns the result if data is null string will be empty
+  return result;
+}
